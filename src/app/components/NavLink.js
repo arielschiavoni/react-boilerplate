676ab250208b51d321010da19b366d770489c594
Ariel Schiavoni <a.schiavoni@2vizcon.com>
2016-08-31 14:48:58 +0200

    Add styles in js

--- src/app/components/NavLink.js
+++ src/app/components/NavLink.js
@@ -3,6 +3,7 @@
 import React from 'react';
 import { Link, IndexLink } from 'react-router';
 import { StyleSheet, css } from 'aphrodite';
+import { colors } from '../styles';
 
 type Props = {
   index?: bool
@@ -22,7 +23,7 @@ const styles = StyleSheet.create({
     textDecoration: 'none'
   },
   activeLink: {
-    color: '#3B9167',
+    color: colors.textSecondary,
     textDecoration: 'underline'
   }
 });
---
+++ src/app/styles/index.js
@@ -0,0 +1,19 @@
+export const colors = {
+  bgPrimary: '#4D4349',
+  bgSecondary: '#A29DA0',
+  textPrimary: '#4FC1E9',
+  textSecondary: '#AEDD4B',
+};
+
+export const spacing = {
+  tiny: '4px'
+};
+
+export const viewport = {
+  upToSmall: '@media only screen and (max-width : 518px)',
+  upToMedium: '@media only screen and (max-width : 765px)',
+  upToBig: '@media only screen and (max-width : 1023px)',
+  atLeastSmall: '@media only screen and (min-width : 519px)',
+  atLeastMedium: '@media only screen and (min-width : 766px)',
+  atLeastBig: '@media only screen and (min-width : 1024px)'
+};
